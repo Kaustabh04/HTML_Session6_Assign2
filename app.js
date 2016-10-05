09e41bbf61ae860c66a8ef16459664564ee9fc41
Kaustabh04 <kaustabh04@gmail.com>
2016-10-06 00:21:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+  // Get the highest value
+  
+  var getHighestNumb = Math.max(100,200,300);
+  
+  // Print in the console by Math.max 
+  
+  console.log("The highest number is " + getHighestNumb);
+  
+  var a = 10;
+  var b = 40;
+  var c = 25;
+  
+  // Using second way to print in the console by comparing each number 
+  
+  if(a>b && a>c){
+	  console.log("'a' is greatest");
+  }
+  else if (b>c && b>a){
+	  console.log("'b' is greatest");
+  }
+  else{
+	  console.log("'c' is greatest");
+  }
